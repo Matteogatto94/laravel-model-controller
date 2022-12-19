d74d30c0eded32695ef8bd5e81664f92a507e839
Matteogatto94 <cosenza94@live.it>
2022-12-19 14:49:56 +0100

    add header and footer and jumbotron

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Laravel-Model-Controller @yield('title', 'Welcome')</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -18,6 +18,27 @@
 <body>
 
     <header>
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">Movies</a>
+                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#mainMenu" aria-controls="mainMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="mainMenu">
+                    <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
     </header>
     <!-- /header -->
@@ -27,8 +48,24 @@
     </main>
     <!-- /main -->
 
-    <footer>
+    <footer class="bg-dark text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="logo">
+                        <h2 class="text-uppercase">Laravel-Model-Controller</h2>
+                    </div>
+                </div>
 
+                <div class="col">
+                    <h3>Legal</h3>
+                    <ul class="list-unstyled">
+                        <li>Privacy & Policy</li>
+                        <li>Terms & Conditions</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </footer>
     <!-- /footer -->
 
