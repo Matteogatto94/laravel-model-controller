4b1bfa39b0afc5cb295e2ccce61e2a1efb4c8bf8
Matteogatto94 <cosenza94@live.it>
2022-12-19 15:50:33 +0100

    add database and save in a variables

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Guest;
 
+use App\Models\Movie;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $movies = Movie::all();
+        return view('welcome', compact('movies'));
     }
 
     public function about()
